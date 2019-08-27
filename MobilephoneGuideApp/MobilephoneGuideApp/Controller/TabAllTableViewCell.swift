7f8f4ab7065bfeb3332c9cbc33d4afbf1bc8ddde
Aphiwit1 <Aphiwit.boon@gmail.com>
2019-08-27 15:10:01 +0700

    [Add] tableviewCell for tabAll page

---
+++ MobilephoneGuideApp/MobilephoneGuideApp/Controller/TabAllTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TabAllTableViewCell.swift
+//  MobilephoneGuideApp
+//
+//  Created by AphiwitSCB on 27/8/2562 BE.
+//  Copyright © 2562 OnceKB. All rights reserved.
+//
+
+import UIKit
+
+class TabAllTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MobilephoneGuideApp/MobilephoneGuideApp/Controller/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MobilephoneGuideApp
+//
+//  Created by AphiwitSCB on 27/8/2562 BE.
+//  Copyright © 2562 OnceKB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tabAllCell") as? TabAllTableViewCell
+        
+        return cell!
+    }
+
+
+}
+
